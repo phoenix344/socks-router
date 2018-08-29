a8893537a7833a149493baf6750feab68cf56e86
EntropyFBC49934 <mario.stoecklein@gmail.com>
2018-08-30 00:00:45 +0200

    a build seems like a good idea

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,13 @@
-import { SocksRoute } from './interfaces/SocksRoute';
 import { SocksConnectionCallback } from "./interfaces/SocksConnectionCallback";
+import { SocksAcceptCallback } from "./interfaces/SocksAcceptCallback";
+import { SocksDenyCallback } from "./interfaces/SocksDenyCallback";
+import { SocksProxyInfo } from "./interfaces/SocksProxyInfo";
+import { SocksRoute } from "./interfaces/SocksRoute";
+import { SocksRouteInitialize } from "./interfaces/SocksRouteInitialize";
+import { SocksRouteInterceptor } from "./interfaces/SocksRouteInterceptor";
+import { SocksRouteValidator } from "./interfaces/SocksRouteValidator";
+import { isUriValid } from "./util/isUriValid";
+export { SocksConnectionCallback, SocksAcceptCallback, SocksDenyCallback, SocksProxyInfo, SocksRoute, SocksRouteInitialize, SocksRouteInterceptor, SocksRouteValidator, isUriValid };
 export declare class Router {
     private routes;
     constructor(routes?: SocksRoute[]);
